54a1f51c97dae2848e7102958a4978d25c05f6cc
RaviShekharThakur <ravissj10@gmail.com>
2024-03-27 13:40:08 +0530

    used Cryptojs to AES Encrypt password and matched after decrypting for login

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,24 +1,30 @@
 import User from "@/models/User";
 import connectDb from "@/middleware/mongoose";
+import CryptoJS from 'crypto-js';
 
 const handler = async (req, res) => {
-    if (req.method == "POST") {
-        console.log(req.body)
-        let user = await User.findOne({ "email": req.body.email })
-        if (user) {
-            if (req.body.email == user.email && req.body.password == user.password) {
-                res.status(200).json({ success: true, email: user.email, name: user.name })
+    if (req.method === "POST") {
+        console.log(req.body);
+        try {
+            let user = await User.findOne({ "email": req.body.email });
+            if (user) {
+                const bytes = CryptoJS.AES.decrypt(user.password, 'secret key 123');
+                let decryptedPassword = bytes.toString(CryptoJS.enc.Utf8);
+                if (req.body.password === decryptedPassword) {
+                    res.status(200).json({ success: true, email: user.email, name: user.name });
+                } else {
+                    res.status(200).json({ success: false, error: "Incorrect Credentials" });
+                }
+            } else {
+                res.status(200).json({ success: false, error: "No user found" });
             }
-            else{
-                res.status(200).json({ success: false, error: "Incorrect Credentials"})
-            }
+        } catch (error) {
+            console.error("Error:", error);
+            res.status(500).json({ success: false, error: "Internal Server Error" });
         }
-        else{
-            res.status(200).json({ success: false, error: "No user found" })
-        }
+    } else {
+        res.status(400).json({ error: "This method is not allowed" });
     }
-    else {
-        res.status(400).json({ error: "this method is not allowed" })
-    }
-}
-export default connectDb(handler)
+};
+
+export default connectDb(handler);
